128300184cc37edaa13540b6ce60059506ffb53d
Vinícius Neto <viniciusbsneto@gmail.com>
2018-09-24 03:46:07 -0300

    Delete quadrado.py

--- quadrado.py
+++
@@ -1,7 +0,0 @@
-def main():
-	lado = float(input("Digite o valor correspondente ao lado de um quadrado: "))
-	area = lado ** 2
-	perimetro = 4 * lado
-	print("perímetro: %.2f - área: %.2f" %(perimetro, area))
-	
-main()
